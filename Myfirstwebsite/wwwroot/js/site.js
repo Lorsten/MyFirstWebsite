8579ba21235144bac574c0f467db4e49c765a760
Olof Andersson <olof.andersson126@outlook.com>
2021-01-25 19:36:23 +0100

    First push of asp.net core website

---
+++ Myfirstwebsite/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.getElementById("mobilemenu").addEventListener("click", function () {
+    let element = document.querySelector("#wrapper nav");
+    element.classList.toggle("active");
+    //Change aria-expended if false to true
+    if (this.getAttribute("aria-expanded") == "false") {
+        this.setAttribute("aria-expanded", "true");
+    }
+    else {
+        this.setAttribute("aria-expanded", "false");
+    }
+});
